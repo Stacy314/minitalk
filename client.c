568f0df93f8e5f58d9315145f4a907ae8288c406
Anastasiia <nestea413@gmail.com>
2024-09-21 16:07:13 +0200

    ver2

--- client.c
+++ client.c
@@ -12,33 +12,32 @@
 
 #include "minitalk.h"
 
-void send_bit(pid_t server_pid, int bit)
+void	send_act(int sig, siginfo_t *info, void *context)
 {
-    if (bit == 1)
-        kill(server_pid, SIGUSR1);
-    else
-        kill(server_pid, SIGUSR2);
-}
+	char	c;
 
-void send_next_bit(pid_t server_pid, siginfo_t *info, void *context)
-{
-    static t_message mes;
-
-    (void)sig;
-    (void)context;
-
-    if (mes.counter == 0) {  // All bits of current char sent, move to next
-        if (*mes.data == '\0') {  // If end of string, terminate
-            exit(0);
-        }
-        mes.data++;
-        mes.counter = 7;
-    } else {
-        mes.counter--;
-    }
-
-    // Send the next bit
-    send_bit(server_pid, (*mes.data >> mes.counter) & 0x01);
+	(void)sig;
+	(void)context;
+	c = *g_mes.data;
+	if ((c >> g_mes.counter) & 0x01)
+	{
+		if (kill (info->si_pid, SIGUSR1) < 0)
+			ft_printf("Fatal error: kill\n");
+	}
+	else
+	{
+		if (kill(info->si_pid, SIGUSR2) < 0)
+			ft_printf("Fatal error: kill\n");
+	}
+	if (g_mes.counter == 0)
+	{
+		if (!c)
+			exit(0);
+		g_mes.data++;
+		g_mes.counter = 7;
+	}
+	else
+		g_mes.counter--;
 }
 
 // int	check_pid(char *c_pid)
@@ -52,68 +51,45 @@ void send_next_bit(pid_t server_pid, siginfo_t *info, void *context)
 // 		return (0);
 // 	return (1);
 // }
-
 int setup_signal_handlers(void)
 {
     struct sigaction send;
-    //struct sigaction empty;
+    struct sigaction empty;
 
     // Set up handler for SIGUSR1
-    send.sa_sigaction = send_next_bit;
+    send.sa_sigaction = send_act;
     send.sa_flags = SA_SIGINFO;
-    sigemptyset(&send.sa_mask); //new
     if (sigaction(SIGUSR1, &send, NULL) < 0)
     {
-        ft_printf("Error setting up signal handlers\n");
+        ft_printf("Fatal error: sigaction\n");
         return (1);
     }
 
-    // // Set up handler for SIGUSR2
-    // empty.sa_sigaction = send_act;
-    // empty.sa_flags = SA_SIGINFO;
-    // if (sigaction(SIGUSR2, &empty, NULL) < 0)
-    // {
-    //     ft_printf("Fatal error: sigaction\n");
-    //     return (1);
-    // }
+    // Set up handler for SIGUSR2
+    empty.sa_sigaction = send_act;
+    empty.sa_flags = SA_SIGINFO;
+    if (sigaction(SIGUSR2, &empty, NULL) < 0)
+    {
+        ft_printf("Fatal error: sigaction\n");
+        return (1);
+    }
 
     return (0); // Success
 }
 
-int validate_pid(char *pid_str)
-{
-    int i = 0;
-
-    while (pid_str[i]) {
-        if (!ft_isdigit(pid_str[i]))
-            return (0);  // Not a valid number
-        i++;
-    }
-    return (1);  // Valid PID
-}
-
 int main(int ac, char **av)
 {
-    pid_t server_pid;
-
     if (ac != 3)
     {
         ft_printf("Usage: ./client [server_pid] [value]\n");
         return (1);
     }
-    if (!validate_pid(argv[1]))
-    {
-        ft_printf("Invalid PID\n");
-        return (1);
-    }
-    server_pid = ft_atoi(argv[1]);
-
     if (setup_signal_handlers() != 0)
         return (1); // Error in signal setup
-    t_message mes;
-    mes.data = argv[2];
-    mes.counter = 7;
-    send_bit(server_pid, (*mes.data >> mes.counter) & 0x01);  // Send first bit
+    g_mes.counter = 7;
+    g_mes.data = av[2];
+    g_mes.data[ft_strlen(g_mes.data)] = '\0';
+
     // Check server PID
     // if (!check_pid(av[1]))
     // {
@@ -122,11 +98,11 @@ int main(int ac, char **av)
     // }
 
     // Send initial signal to server
-    // if (kill(ft_atoi(av[1]), SIGUSR1) < 0)
-    // {
-    //     ft_printf("Fatal error: kill\n");
-    //     return (1);
-    // }
+    if (kill(ft_atoi(av[1]), SIGUSR1) < 0)
+    {
+        ft_printf("Fatal error: kill\n");
+        return (1);
+    }
     while (1)
         pause();
     return (0);
--- server.c
+++ server.c
@@ -12,96 +12,83 @@
 
 #include "minitalk.h"
 
-void	handle_character_output(unsigned int c)
+void	handle_character_output(int c)
 {
-	if (c == 0);
+	if (g_mes.value == 0)  // Якщо кінець строки, вивести новий рядок
 		ft_printf("\n");
 	else
-		ft_putchar_fd(c, 1);
+		ft_putchar_fd(c, 1);  // Використовуємо вашу функцію для виводу символу
+	g_mes.counter = 7;
+	g_mes.value = 0;
 }
 
-// long int	ft_pow(long int x, unsigned int n)
-// {
-// 	if (n == 0)
-// 		return (1);
-// 	else if (n == 1)
-// 		return (x);
-// 	else if (n % 2 == 0)
-// 		return (ft_pow(x * x, n / 2));
-// 	else
-// 		return (ft_pow(x * x, n / 2) * x);
-// }
+long int	ft_pow(long int x, unsigned int n)
+{
+	if (n == 0)
+		return (1);
+	else if (n == 1)
+		return (x);
+	else if (n % 2 == 0)
+		return (ft_pow(x * x, n / 2));
+	else
+		return (ft_pow(x * x, n / 2) * x);
+}
 
 void	one_act(int sig, siginfo_t *info, void *context)
 {
-	static t_message mes = { .counter = 7, .value = 0 };
 	(void)sig;
 	(void)context;
-	// Set the corresponding bit using bitwise OR
-    mes.value |= (1 << mes.counter);
-	//Decrement counter and check if full byte is received
-    if (mes.counter == 0) {
-        handle_character_output(mes.value);
-        mes.counter = 7;
-        mes.value = 0;  // Reset for next character
-    } else {
-        mes.counter--;
-    }
-
-    // Acknowledge the client
-    if (kill(info->si_pid, SIGUSR1) < 0)
-        ft_printf("Error sending signal to client\n");
+	if (g_mes.counter == 7)
+	{
+		if (kill(info->si_pid, SIGUSR1) < 0)
+		{
+			ft_printf("Fatal error: kill\n");
+			exit (1);
+		}
+		return ;
+	}
+	g_mes.value += ft_pow(2, g_mes.counter);
+	if (g_mes.counter == 0)
+		handle_character_output(g_mes.value);
+	else
+		g_mes.counter--;
+	kill (info->si_pid, SIGUSR1);
 }
 
 void	zero_act(int sig, siginfo_t *info, void *context)
 {
-	static t_message mes = { .counter = 7, .value = 0 };
 	(void)sig;
 	(void)context;
-// No need to set bit, just decrement counter
-    if (mes.counter == 0) {
-        handle_character_output(mes.value);
-        mes.counter = 7;
-        mes.value = 0;
-    } else {
-        mes.counter--;
-    }
-
-    // Acknowledge the client
-    if (kill(info->si_pid, SIGUSR1) < 0)
-        ft_printf("Error sending signal to client\n");
-}
-
-int setup_signal_handlers(void)
-{
-    struct sigaction one;
-    struct sigaction zero;
-
-    one.sa_sigaction = one_act;
-    one.sa_flags = SA_SIGINFO;
-    sigemptyset(&one.sa_mask);
-
-    zero.sa_sigaction = zero_act;
-    zero.sa_flags = SA_SIGINFO;
-    sigemptyset(&zero.sa_mask);
-
-    if (sigaction(SIGUSR1, &one, NULL) < 0 || sigaction(SIGUSR2, &zero, NULL) < 0) {
-        ft_printf("Error setting up signal handlers\n");
-        return (1);
-    }
-
-    return (0);  // Success
+	if (g_mes.counter == 0)
+		handle_character_output(g_mes.value);
+	else
+		g_mes.counter--;
+	kill (info->si_pid, SIGUSR1);
 }
 
 int	main(void)
 {
-    ft_printf("Server PID: %d\n", getpid());
-    
-    if (setup_signal_handlers() != 0)
-        return (1);  // Exit on signal setup failure
+	struct sigaction	one;
+	struct sigaction	zero;
 
-    while (1)
-        pause();  // Wait for incoming signals
-
-    return (0);
+	ft_printf("Server PID: %d\n", getpid());
+	one.sa_sigaction = one_act;
+	one.sa_flags = SA_SIGINFO; 
+	zero.sa_sigaction = zero_act;
+	zero.sa_flags = SA_SIGINFO;
+	g_mes.counter = 7;
+	g_mes.value = 0;
+	if (sigaction(SIGUSR1, &one, NULL) < 0)
+	{
+		ft_printf("Fatal error: sigaction\n");
+		return (1);
+	}
+	if (sigaction(SIGUSR2, &zero, NULL) < 0)
+	{
+		ft_printf("Fatal error: sigaction\n");
+		return (1);		
+	}
+	while (1)
+		pause();
+	return (0);
 }
